edb8cf1de6f88495e0f47129e22955ec395d8419
Anas <anasshah668@gmail.com>
2021-10-24 02:14:32 +0500

    initial

---
+++ config.py
@@ -0,0 +1,11 @@
+params = {
+    "local_server": "True",
+    "local_uri": "mysql://root:@localhost/mobiletech",
+    "prod_uri": "mysql://root:@localhost/mobiletech",
+    "fb_url": "https://facebook.com/codingthunder",
+    "gh_url": "https://github.com/codingthunder",
+    "tw_url": "https://twitter.com/codingthunder",
+    "gmail_user": "*** Enter your email here ***",
+    "gmail_password": "*** Enter your password here *** "
+  }
+
